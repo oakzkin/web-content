b53412bcc4cbaef5ed5123eb84ad51d531995168
Oakzkin <r.intouchpong@gmail.com>
2019-06-23 14:04:40 +0700

    add auth service and other

--- services/user/index.js
+++ services/user/index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongo = require('./db/mongo')
 const jwt = require('jsonwebtoken')
+const jwtVerify = require('express-jwt')
 
 const url = process.env.MONGO_URL
 const dbName = process.env.DB_NAME
 const jwtSecret = process.env.JWT_SECRET
+
+const auth = jwtVerify({ secret: jwtSecret })
 
 let db = null
 mongo.connect(url, dbName, (instance) =>{
@@ -33,34 +36,22 @@ app.get('/', (req, res) => {
 
 
 
-app.get('/:id', (req, res) => {
-    const id = req.params.id
-    db.collection('account').findOne({ _id: mongo.toID}, (err, result) => {
-        if(err) {
-            return res.json({
-                error: err.message
-            })
-        }
-        return res.json({
-            data: result
-        })
-    })
-})
+
 
 app.post('/auth', (req, res) => {
     const username = req.body.username
     const password = req.body.password
 
     const filter = { username: username}
-    console.log(filter)
-
     
+
+
     db.collection('account').findOne(filter, (err, result) => {
-        console.log(err, result)
+        
         if(err){
             return res.json({err})
         }
-        if(!result.username){
+        if(!result){
             return res.json({ message: 'user not found'})
         }
 
@@ -77,8 +68,36 @@ app.post('/auth', (req, res) => {
 })
 
 
+app.get('/getInfo', auth , (req, res) => {
+    return res.json({
+        data: req.user
+    })
+    
 
-app.post('/:id', (req, res) => {
+    
+
+})
+
+
+
+
+
+
+app.get('/u/:id', (req, res) => {
+    const id = req.params.id
+    db.collection('account').findOne({ _id: mongo.toID}, (err, result) => {
+        if(err) {
+            return res.json({
+                error: err.message
+            })
+        }
+        return res.json({
+            data: result
+        })
+    })
+})
+
+app.post('/u/:id', (req, res) => {
     const id = req.params.id
     const password = req.body.password
 
@@ -127,6 +146,12 @@ app.post('/', (req,res) => {
 /*
     */
 })
+
+app.use(function (err, req, res, next) {
+    if (err.name === 'UnauthorizedError') {
+      res.status(401).send('invalid token...');
+    }
+  });
 
 
 
