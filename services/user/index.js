647adc09b9603769376def7e4109da1fe79141d7
Oakzkin <r.intouchpong@gmail.com>
2019-08-18 13:26:31 +0700

    add login logout function

--- services/user/index.js
+++ services/user/index.js
@@ -1,164 +1,153 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongo = require('./db/mongo')
-const jwt = require('jsonwebtoken')
-const jwtVerify = require('express-jwt')
-const pw = require('./utils/password')
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongo = require("./db/mongo");
+const jwt = require("jsonwebtoken");
+const jwtVerify = require("express-jwt");
+const pw = require("./utils/password");
 
-const url = process.env.MONGO_URL
-const dbName = process.env.DB_NAME
-const jwtSecret = process.env.JWT_SECRET
+const url = process.env.MONGO_URL;
+const dbName = process.env.DB_NAME;
+const jwtSecret = process.env.JWT_SECRET;
 
-const auth = jwtVerify({ secret: jwtSecret })
+const auth = jwtVerify({ secret: jwtSecret });
 
-let db = null
-mongo.connect(url, dbName, (instance) =>{
-    db = instance
-})
+let db = null;
+mongo.connect(url, dbName, instance => {
+  db = instance;
+});
 
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-
-const app = express()
-app.use(bodyParser.urlencoded({ extended: false}))
-app.use(bodyParser.json())
-
-app.get('/', (req, res) => {
-        db.collection('account').find({}).toArray((err, result) => {
-            if(err) {
-                return res.json({
-                    error: err.message
-                })
-            }
-            return res.json({
-                data: result
-            })
-        })
-})
-
-
-
-
-
-app.post('/auth', (req, res) => {
-    const username = req.body.username
-    const password = req.body.password
-
-    const filter = { username: username}
-    
-
-
-    db.collection('account').findOne(filter, (err, result) => {
-        
-        if(err){
-            return res.json({err})
-        }
-        if(!result){
-            return res.json({ message: 'user not found'})
-        }
-
-        if(pw.compare(password, result.password)){
-            const token = jwt.sign({
-                username: result.username
-            }, jwtSecret, { expiresIn: '2h'})
-            return res.json({ token: token})
-        }
-
-        return res.json({ message: 'Invalid Username'})
-    })
-
-})
-
-
-app.get('/getInfo', auth , (req, res) => {
-    return res.json({
-        data: req.user
-    })
-    
-
-    
-
-})
-
-
-
-
-
-
-app.get('/u/:id', (req, res) => {
-    const id = req.params.id
-    db.collection('account').findOne({ _id: mongo.toID}, (err, result) => {
-        if(err) {
-            return res.json({
-                error: err.message
-            })
-        }
+app.get("/", (req, res) => {
+  db.collection("account")
+    .find({})
+    .toArray((err, result) => {
+      if (err) {
         return res.json({
-            data: result
-        })
-    })
-})
+          error: err.message
+        });
+      }
+      return res.json({
+        data: result
+      });
+    });
+});
 
-app.post('/u/:id', (req, res) => {
-    const id = req.params.id
-    const password = req.body.password
+app.post("/auth", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-    const filter = { _id: mongo.toID(id) }
-    const update = { '$set': { password: password} }
+  const filter = { username: username };
 
-    db.collection('account').updateOne(filter, update, (err, result) => {
-        return res.json({
-            data: result.result.ok > 0
-        })
-    })
-})
-
-app.post('/u', (req,res) => {
-    
-    const username = req.body.username
-    const password = pw.hash(req.body.password)
-    const accountCollection = db.collection('account')
-
-    console.log('username', username)
-
-    accountCollection.findOne({
-        'username': username
-    }, (err, document) => {
-        if(document){
-            return res.json({
-                status: false,
-                message: 'User Already Exists'
-            })
-        }
-
-        accountCollection.insertOne({
-            username: username,
-            password: password
-        }, (err, result) => {
-            if(err !== null) {
-                return res.json({
-                    status: false,
-                    message: 'Cannot Create user (DB error)'
-                })
-            }
-            return res.json({
-                status: result.result.n === 1,
-                message: 'user ' + username + ' was created'
-            })
-        })
-        
-    })
-/*
-    */
-})
-
-app.use(function (err, req, res, next) {
-    if (err.name === 'UnauthorizedError') {
-      res.status(401).send('invalid token...');
+  db.collection("account").findOne(filter, (err, result) => {
+    if (err) {
+      return res.json({ message: "database error", err: "DATABASE_ERROR" });
     }
+
+    if (!result) {
+      return res.json({ message: "user not found", err: "USER_NOT_FOUND" });
+    }
+
+    if (pw.compare(password, result.password)) {
+      const token = jwt.sign(
+        {
+          username: result.username
+        },
+        jwtSecret,
+        { expiresIn: "2h" }
+      );
+      return res.json({ token: token });
+    }
+
+    return res.json({ message: "Invalid Username", err: "INVALID_PASSWORD" });
   });
+});
 
+app.get("/getInfo", auth, (req, res) => {
+  return res.json({
+    data: req.user
+  });
+});
 
+app.get("/u/:id", (req, res) => {
+  const id = req.params.id;
+  db.collection("account").findOne({ _id: mongo.toID }, (err, result) => {
+    if (err) {
+      return res.json({
+        error: err.message
+      });
+    }
+    return res.json({
+      data: result
+    });
+  });
+});
+
+app.post("/u/:id", (req, res) => {
+  const id = req.params.id;
+  const password = req.body.password;
+
+  const filter = { _id: mongo.toID(id) };
+  const update = { $set: { password: password } };
+
+  db.collection("account").updateOne(filter, update, (err, result) => {
+    return res.json({
+      data: result.result.ok > 0
+    });
+  });
+});
+
+app.post("/u", (req, res) => {
+  const username = req.body.username;
+  const password = pw.hash(req.body.password);
+  const accountCollection = db.collection("account");
+
+  console.log("username", username);
+
+  accountCollection.findOne(
+    {
+      username: username
+    },
+    (err, document) => {
+      if (document) {
+        return res.json({
+          status: false,
+          message: "User Already Exists"
+        });
+      }
+
+      accountCollection.insertOne(
+        {
+          username: username,
+          password: password
+        },
+        (err, result) => {
+          if (err !== null) {
+            return res.json({
+              status: false,
+              message: "Cannot Create user (DB error)"
+            });
+          }
+          return res.json({
+            status: result.result.n === 1,
+            message: "user " + username + " was created"
+          });
+        }
+      );
+    }
+  );
+  /*
+   */
+});
+
+app.use(function(err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).send("invalid token...");
+  }
+});
 
 app.listen(process.env.PORT, () => {
-    console.log('service user listening at port: '+ process.env.PORT)
-})
+  console.log("service user listening at port: " + process.env.PORT);
+});
---
+++ services/web/index.js
@@ -0,0 +1,156 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const fetch = require('node-fetch')
+const jwtVerify = require('express-jwt')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+const jwtSecret = process.env.JWT_SECRET
+const auth = jwtVerify({
+  secret: jwtSecret,
+  credentialsRequired: false,
+  getToken: req => {
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.split(' ')[0] === 'Bearer'
+    ) {
+      return req.headers.authorization.split(' ')[1]
+    } else if (req.query && req.query.token) {
+      return req.query.token
+    } else if (req.cookies && req.cookies.jwt) {
+      return req.cookies.jwt
+    }
+    return null
+  }
+})
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use('/public', express.static('public'))
+
+//-----------------------------------------------------------
+
+const fs = require('fs') // this engine requires the fs module (fs = file system)
+app.engine('html', (filePath, options, callback) => {
+  // define the template engine
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) return callback(err)
+    // this is an extremely simple template engine
+
+    let rendered = content.toString()
+    for (let key in options.data || {}) {
+      rendered = rendered.replace('{{ ' + key + ' }}', options.data[key])
+    }
+
+    return callback(null, rendered)
+  })
+})
+app.set('views', './views') // specify the views directory
+app.set('view engine', 'html') // register the template engine
+
+const serviceArticle = process.env.ARTICLE_API_PATH
+
+const renderArticleCard = article => {
+  return `
+  <a href="/${article._id}">
+    <div class="card">
+      <div class="card-body">
+        <h2>${article.title}</h2>
+        <div class="card-info">
+          Update : <span>${article.updateAt}</span>
+        </div>
+      </div>
+    </div>
+  </a>  
+  `
+}
+
+//-------------------------------------------------
+
+app.get('/', (req, res) => {
+  fetch(serviceArticle)
+    .then(res => res.json())
+    .then(data => {
+      const articles = data.list.map(renderArticleCard).join('')
+
+      res.render('index', {
+        data: {
+          test: 'test template',
+          articles: articles
+        }
+      })
+    })
+})
+
+app.get('/:id', auth, (req, res) => {
+  
+  const user = req.user
+
+
+  fetch(`${serviceArticle}/${req.params.id}`)
+    .then(res => res.json())
+    .then(json => {
+      const article = json.data
+      res.render('article', {
+        data: {
+          title: article.title,
+          content: article.content,
+          author: article.username,
+          isLoginClass : user ? '' : 'd-none',
+          isNotLoginClass : user ? 'd-none' : '',
+          username: user ? user.username : ''
+        }
+      })
+    })
+})
+
+app.post('/login', (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+
+  const body = JSON.stringify({
+    username: username,
+    password: password
+  })
+
+  fetch('http://user:3000/auth', {
+    method: 'POST',
+    body: body,
+    headers: { 'Content-Type': 'application/json' }
+  })
+    .then(r => r.json())
+    .then(data => {
+      if (!data.token) {
+        let errMessage = ''
+        switch (data.err) {
+          case 'USER_NOT_FOUND':
+          case 'INVALID_PASSWORD':
+            errMessage = 'Username หรือ Password ผิดพลาด'
+            break
+          default:
+            errMessage = 'เกิดข้อผิดพลาด กรุณาลองอีกครั้ง'
+            break
+        }
+        res.json({
+          status: false,
+          message: errMessage
+        })
+      }
+      return res.json({
+        status: true,
+        token: data.token
+      })
+    })
+    .catch(err =>
+      res.json({
+        status: false,
+        message: 'Unexpected Error'
+      })
+    )
+})
+
+app.listen(process.env.PORT, () => {
+  console.log('service frontend start')
+})
